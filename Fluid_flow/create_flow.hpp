79d154d510f076ff096de0b41a99c7effa5a088d
daRoyalCacti <srdrmrdave@gmail.com>
2021-08-19 18:01:08 +1000

    Removed debug code

--- Fluid_flow/create_flow.hpp
+++ Fluid_flow/create_flow.hpp
@@ -110,31 +110,6 @@ void create_boundary_points(boundary_points<N,M,P> &bound) {
     }
 }
 
-//delete start
-#include <string_view>
-
-template <typename T>
-constexpr auto type_name() noexcept {
-    std::string_view name = "Error: unsupported compiler", prefix, suffix;
-#ifdef __clang__
-    name = __PRETTY_FUNCTION__;
-  prefix = "auto type_name() [T = ";
-  suffix = "]";
-#elif defined(__GNUC__)
-    name = __PRETTY_FUNCTION__;
-    prefix = "constexpr auto type_name() [with T = ";
-    suffix = "]";
-#elif defined(_MSC_VER)
-    name = __FUNCSIG__;
-  prefix = "auto __cdecl type_name<";
-  suffix = ">(void) noexcept";
-#endif
-    name.remove_prefix(prefix.size());
-    name.remove_suffix(suffix.size());
-    return name;
-}
-//delete end
-
 
 template<unsigned N, unsigned M, unsigned P, typename T>
 void write_vec(const big_vec<N,M,P,T>& v, const char* file_loc) {
