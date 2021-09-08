e949c45e05ad9be9fa37d7f72f3ec2771a49ac27
daRoyalCacti <srdrmrdave@gmail.com>
2021-09-08 17:06:30 +1000

    Averaging times now produces a pie chart

--- DEBUG/average_times.m
+++ DEBUG/average_times.m
@@ -1,20 +1,24 @@
-function [t, s, p, b, vx, vy, vz] = average_times(file_loc)
-    fileID = fopen(file_loc, 'r');
-    data = fscanf(fileID, '%f %f %f %f %f %f %f %f');
-    fclose(fileID);
-    all_s =  data(2:8:end);
-    all_p =  data(3:8:end);
-    all_b =  data(4:8:end);
-    all_vx = data(5:8:end);
-    all_vy = data(6:8:end);
-    all_vz = data(7:8:end);
-    all_t =  data(8:8:end);
-    
-    s = mean(all_s);
-    p = mean(all_p);
-    b = mean(all_b);
-    vx = mean(all_vx);
-    vy = mean(all_vy);
-    vz = mean(all_vz);
-    t = mean(all_t);
-end
+file_loc = "./times.txt";
+fileID = fopen(file_loc, 'r');
+data = fscanf(fileID, '%f %f %f %f %f %f %f %f %f');
+fclose(fileID);
+all_s =  data(2:9:end);
+all_p =  data(3:9:end);
+all_b =  data(4:9:end);
+all_vx = data(5:9:end);
+all_vy = data(6:9:end);
+all_vz = data(7:9:end);
+all_mesh =  data(8:9:end);
+all_t =  data(9:9:end);
+
+s = mean(all_s);
+p = mean(all_p);
+b = mean(all_b);
+vx = mean(all_vx);
+vy = mean(all_vy);
+vz = mean(all_vz);
+t = mean(all_t);
+m = mean(all_mesh);
+
+pie([m, s, p, b, vx, vy, vz],[1,0,0,0,0,0,0], {'updating mesh', 'making s', 'solving p', 'making b', 'solving vx', 'solving vy', 'solving vz'})
+text(-0.5,-1.3, ['average time = ', num2str(t), 's'])
