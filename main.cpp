1ecdab00b816601e1889a8c799803cff9d71c5df
daRoyalCacti <srdrmrdave@gmail.com>
2021-09-08 19:03:25 +1000

    Made box lighter and spin

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ int main() {
     std::vector<vec3> vels;
     std::vector<vec3> norms;
 
+    constexpr double mass1 = 0.01;
+
     const vec3 vel_cm = vec3(0.75, 0, 0);
     const vec3 w{};
 
@@ -28,7 +30,7 @@ int main() {
         for (int j = 0; j < 2; j++) {
             for (int k = 0; k < 2; k++) {
                 pos.emplace_back( (2*i-1)*wx/4 + x_mid, (2*j-1)*wy/4 + y_mid, (2*k-1)*wz/4 + z_mid );
-                mass.push_back(1);
+                mass.push_back(mass1);
                 norms.emplace_back((2*i-1), 0, 0);
             }
         }
@@ -39,7 +41,7 @@ int main() {
         for (int i = 0; i < 2; i++) {
             for (int k = 0; k < 2; k++) {
                 pos.emplace_back( (2*i-1)*wx/4 + x_mid, (2*j-1)*wy/4 + y_mid, (2*k-1)*wz/4 + z_mid );
-                mass.push_back(1);
+                mass.push_back(mass1);
                 norms.emplace_back(0, (2*j-1), 0);
             }
         }
@@ -50,7 +52,7 @@ int main() {
         for (int j = 0; j < 2; j++) {
             for (int i = 0; i < 2; i++) {
                 pos.emplace_back( (2*i-1)*wx/4 + x_mid, (2*j-1)*wy/4 + y_mid, (2*k-1)*wz/4 + z_mid );
-                mass.push_back(1);
+                mass.push_back(mass1);
                 norms.emplace_back(0, 0, (2*k-1));
             }
         }
@@ -66,7 +68,7 @@ int main() {
         inds.push_back(4*i+3);  //upper right
     }
 
-    mesh m(pos, inds, mass, norms, vec3(0.75,0,0), vec3(0));
+    mesh m(pos, inds, mass, norms, vec3(0.75,0,0), vec3(0,1,0));
     body b(m);
     solve_flow<1000, 128, 128, 128>(&b, o, max_t, Re, wx, wy, wz);
 
