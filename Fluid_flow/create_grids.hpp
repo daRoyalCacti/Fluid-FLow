999e2edf5640b3d5301f245ace71907bfa507211
daRoyalCacti <srdrmrdave@gmail.com>
2021-09-11 19:26:29 +1000

    Entire grid now the correct size

--- Fluid_flow/create_grids.hpp
+++ Fluid_flow/create_grids.hpp
@@ -9,14 +9,19 @@
 #include <vector>
 
 void make_enitre_gid(grid &g, const double Wx, const double Wy, const double Wz, const double dx, const double dy, const double dz) {
-    g.x.resize(static_cast<unsigned long>(Wx/dx));
-    g.y.resize(static_cast<unsigned long>(Wy/dy));
-    g.z.resize(static_cast<unsigned long>(Wz/dz));
+    const auto sx = static_cast<unsigned long>(Wx/dx);
+    const auto sy = static_cast<unsigned long>(Wy/dy);
+    const auto sz = static_cast<unsigned long>(Wz/dz);
+
+    const auto s = static_cast<unsigned long>(sx*sy*sz);
+    g.x.resize(s);
+    g.y.resize(s);
+    g.z.resize(s);
 
     unsigned counter = 0;
-    for (unsigned i = 0; i < g.x.size(); i++) {
-        for (unsigned j = 0; j < g.y.size(); j++) {
-            for (unsigned k = 0; k < g.z.size(); k++) {
+    for (unsigned i = 0; i < sx; i++) {
+        for (unsigned j = 0; j < sy; j++) {
+            for (unsigned k = 0; k < sz; k++) {
                 g.x[counter] = i*dx;
                 g.y[counter] = j*dy;
                 g.z[counter] = k*dz;
