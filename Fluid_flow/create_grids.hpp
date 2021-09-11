3d47237a3772bf0351619fb0b9e90dfacb025a1d
daRoyalCacti <srdrmrdave@gmail.com>
2021-09-12 09:30:24 +1000

    Grid now stores a bounding box for itself through storing its min and max element in every direction

--- Fluid_flow/create_grids.hpp
+++ Fluid_flow/create_grids.hpp
@@ -7,11 +7,15 @@
 
 #include "../MyMath/grid.hpp"
 #include <vector>
+#include <unordered_set>
 
 void make_enitre_gid(grid &g, const double Wx, const double Wy, const double Wz, const double dx, const double dy, const double dz) {
     const auto sx = static_cast<unsigned long>(Wx/dx);
     const auto sy = static_cast<unsigned long>(Wy/dy);
     const auto sz = static_cast<unsigned long>(Wz/dz);
+
+    g.mins = {0,0,0};
+    g.maxs = {Wx, Wy, Wz};
 
     const auto s = static_cast<unsigned long>(sx*sy*sz);
     g.x.resize(s);
@@ -55,7 +59,7 @@ void make_enitre_gid(grid &g, const double Wx, const double Wy, const double Wz,
                 } else {
                     back = -1;
                 }
-                if (k != sx-1) {
+                if (k != sz-1) {
                     front = k + 1;
                 } else {
                     back = -1;
@@ -69,5 +73,42 @@ void make_enitre_gid(grid &g, const double Wx, const double Wy, const double Wz,
     }
 
 }
+
+
+void remove_inside_boundary(grid &g, const mesh &model, boundary_normals &norms, mesh_points &m_points) {
+    std::unordered_set<unsigned> inside_indices;
+
+    for (unsigned i = 0; i <= N; ++i) {
+        for (unsigned j = 0; j <= M; ++j) {
+            const auto pos = vel_bc.get_pos(i,j,0);
+            const auto dx = vel_bc.dx(i,j,0);
+            const auto dy = vel_bc.dy(i,j,0);
+            const ray r(vec3(pos.x() + dx/2, pos.y() + dy/2, 0), vec3(0,0,1) );//shoot ray through the middle of a grid point
+            set_BC_mesh_1dir_z(r, inside_indices, norms, m_points, i, j);
+        }
+    }
+
+    for (unsigned i = 0; i <= N; ++i) {
+        for (unsigned k = 0; k <= P; ++k) {
+            const auto pos = vel_bc.get_pos(i,0,k);
+            const auto dx = vel_bc.dx(i,0,k);
+            const auto dz = vel_bc.dz(i,0,k);
+            const ray r(vec3(pos.x() + dx/2, 0, pos.z() + dz/2), vec3(0,1,0) );//shoot ray through the middle of a grid point
+            set_BC_mesh_1dir_y(r, inside_indices, norms, m_points, i, k);
+        }
+    }
+
+    for (unsigned j = 0; j <= M; ++j) {
+        for (unsigned k = 0; k <= P; ++k) {
+            const auto pos = vel_bc.get_pos(0,j,k);
+            const auto dy = vel_bc.dy(0,j,k);
+            const auto dz = vel_bc.dz(0,j,k);
+            const ray r(vec3(0, pos.y() + dy/2, pos.z() + dz/2), vec3(1,0,0) );//shoot ray through the middle of a grid point
+            set_BC_mesh_1dir_x(r, inside_indices, norms, m_points, j, k);
+        }
+    }
+}
+
+
 
 #endif //CODE_CREATE_GRIDS_HPP
--- MyMath/grid.hpp
+++ MyMath/grid.hpp
@@ -6,6 +6,7 @@
 #define CODE_GRID_HPP
 
 #include <vector>
+#include <limits>
 
 //stores the neighbours of a grid point
 // -1 if no neighbour
@@ -14,15 +15,18 @@ struct grid_relation {
 };
 
 //grids must be axis aligned
+// grid[i] corresponds to the interval x[i]+dx, y[i]+dy, z[i]+dz
 struct grid {
     std::vector<double> x, y, z;
     double dx, dy, dz;
     std::vector<grid_relation> r{};
+    vec3 mins, maxs;    //min = (minx, miny, minz)
 
 
     grid() : dx{}, dy{}, dz{} {};
-    grid(const std::vector<double> &x_, const std::vector<double> &y_, const std::vector<double> &z_, const double dx_, const double dy_, const double dz_)
-        : x(x_), y(y_), z(z_), dx(dx_), dy(dy_), dz(dz_) {
+    grid(const std::vector<double> &x_, const std::vector<double> &y_, const std::vector<double> &z_, const double dx_, const double dy_, const double dz_,
+         const double minx, const double miny, const double minz, const double maxx, const double maxy, const double maxz)
+        : x(x_), y(y_), z(z_), dx(dx_), dy(dy_), dz(dz_), mins(minx, miny, minz), maxs(maxx, maxy, maxz) {
 #ifndef NDEBUG
         bool err = false;
         if (x_.size() != y_.size()) {
@@ -42,6 +46,50 @@ struct grid {
         }
 #endif
     }
+
+    [[nodiscard]] constexpr inline auto get_ind(const vec3& p) const noexcept {
+        //finds the index of the grid that p is in
+        constexpr double inf = std::numeric_limits<double>::max();
+        double min_x = inf, min_y = inf, min_z = inf;
+
+        constexpr unsigned long init_ind = std::numeric_limits<unsigned long>::max(); //some stupid number
+        unsigned long best_ind = init_ind;
+
+        for (unsigned long i = 0; i < x.size(); i++) {
+            const double dist_x = p.x() - x[i];
+            if (dist_x < 0) {   //because the grid ranges from x[i],x[i]+dx being to the left of x[i] means the point is not in the grid
+                continue;
+            }
+            const double dist_y = p.y() - y[i];
+            if (dist_y < 0) {
+                continue;
+            }
+            const double dist_z = p.z() - z[i];
+            if (dist_z < 0) {
+                continue;
+            }
+
+            if (dist_x < min_x || dist_y < min_y || dist_z < min_z) {
+                min_x = dist_x;
+                min_y = dist_y;
+                min_z = dist_z;
+                best_ind = i;
+            }
+
+        }
+
+#ifndef NDEBUG
+        if (best_ind == init_ind) {
+            std::cerr << "point is to the left of the entire grid\n\tpoint = " << p << "\n";
+        } else if (p.x() > x[best_ind]+dx || p.y() > y[best_ind]+dy || p.z() > z[best_ind] + dz ) { //must be an else if because x[best_ind] will error otherwise
+            std::cerr << "point is outside of grid\n\tpoint = " << p << "\n";
+        }
+#endif
+
+        return best_ind;
+
+    }
+
 };
 
 #endif //CODE_GRID_HPP
