f2811a32c8dac2035cbca3c70ebc0366ab9f84cc
daRoyalCacti <srdrmrdave@gmail.com>
2021-09-19 15:00:27 +1000

    Removed unnecessary checks

--- Fluid_flow/update_vecs.hpp
+++ Fluid_flow/update_vecs.hpp
@@ -66,49 +66,33 @@ void update_pressure_BC(const boundary_conditions &BC, big_vec_d &p) {
 
 
                 if (big_dir == 0) { //x direction biggest
-                    if (!p.has_left(i)) {   //forward difference
-                        p(i) = ny/nx* smart_deriv<0,1,0>(p, i)*2*dx/3 + nz/nx* smart_deriv<0,0,1>(p, i)*2*dx/3 - p.move(i,2,0,0)/3 + 4*p.move(i,1,0,0)/3;
-#ifndef NDEBUG
-                        if (!std::isfinite(p(i))) {
-                            std::cerr << "pressure boundary condition returned an infinite value\n";
-                        }
-#endif
-                    } else if (!p.has_right(i)) {   //backward difference
+                    if (!p.has_right(i)) {   //backward difference
                         p(i) =-ny/nx* smart_deriv<0,1,0>(p, i)*2*dx/3 - nz/nx* smart_deriv<0,0,1>(p, i)*2*dx/3 - p.move(i,-2,0,0)/3 + 4*p.move(i,-1,0,0)/3;
 #ifndef NDEBUG
                         if (!std::isfinite(p(i))) {
                             std::cerr << "pressure boundary condition returned an infinite value\n";
                         }
 #endif
-                    } else {    //central difference
-                        //p( p.get_move_ind(i,1,0,0) ) = -ny/nx * smart_deriv<0,1,0>(p, i)*dx - nz/nx * smart_deriv<0,0,1>(p, i)*dx + p.move(i,-1,0,0);
+                    } else {   //forward difference
                         p(i) = ny/nx* smart_deriv<0,1,0>(p, i)*2*dx/3 + nz/nx* smart_deriv<0,0,1>(p, i)*2*dx/3 - p.move(i,2,0,0)/3 + 4*p.move(i,1,0,0)/3;
 #ifndef NDEBUG
-                        if (!std::isfinite(p.move(i,1,0,0))) {
+                        if (!std::isfinite(p(i))) {
                             std::cerr << "pressure boundary condition returned an infinite value\n";
                         }
 #endif
                     }
                 } else if (big_dir == 1) {  //y direction biggest
-                    if (!p.has_down(i)) { //forward difference
-                        p(i) = nx/ny* smart_deriv<1,0,0>(p, i)*2*dy/3 + nz/ny* smart_deriv<0,0,1>(p, i)*2*dy/3 - p.move(i, 0,2,0)/3 + 4*p.move(i,0,1,0)/3;
- #ifndef NDEBUG
-                        if (!std::isfinite(p(i))) {
-                            std::cerr << "pressure boundary condition returned an infinite value\n";
-                        }
-#endif
-                    } else if (!p.has_up(i)) {  //backward difference
+                    if (!p.has_up(i)) {  //backward difference
                         p(i) =-nx/ny* smart_deriv<1,0,0>(p, i)*2*dy/3 - nz/ny* smart_deriv<0,0,1>(p, i)*2*dy/3 - p.move(i,0,-2,0)/3 + 4*p.move(i,0,-1,0)/3;
 #ifndef NDEBUG
                         if (!std::isfinite(p(i))) {
                             std::cerr << "pressure boundary condition returned an infinite value\n";
                         }
 #endif
-                    } else {  //central difference
-                        //p( p.get_move_ind(i, 0,1,0) ) = -nx/ny * smart_deriv<1,0,0>(p, i)*dy - nz/ny * smart_deriv<0,0,1>(p, i)*dy + p.move(i,0,-1,0);
+                    } else { //forward difference
                         p(i) = nx/ny* smart_deriv<1,0,0>(p, i)*2*dy/3 + nz/ny* smart_deriv<0,0,1>(p, i)*2*dy/3 - p.move(i, 0,2,0)/3 + 4*p.move(i,0,1,0)/3;
-#ifndef NDEBUG
-                        if (!std::isfinite(p.move(i,0,1,0))) {
+ #ifndef NDEBUG
+                        if (!std::isfinite(p(i))) {
                             std::cerr << "pressure boundary condition returned an infinite value\n";
                         }
 #endif
@@ -119,25 +103,17 @@ void update_pressure_BC(const boundary_conditions &BC, big_vec_d &p) {
                         std::cerr << "the biggest direction cannot be larger than 2\n";
                     }
 #endif
-                    if (!p.has_front(i)) {  //forward difference
-                        p(i) = ny/nz* smart_deriv<0,1,0>(p, i)*2*dz/3 + nx/nz* smart_deriv<1,0,0>(p, i)*2*dz/3 - p.move(i,0,0,2)/3 + 4*p.move(i,0,0,1)/3;
-#ifndef NDEBUG
-                        if (!std::isfinite(p(i))) {
-                            std::cerr << "pressure boundary condition returned an infinite value\n";
-                        }
-#endif
-                    } else if (!p.has_back(i)) { //backward difference
+                    if (!p.has_back(i)) { //backward difference
                         p(i) =-ny/nz* smart_deriv<0,1,0>(p, i)*2*dz/3 - nx/nz* smart_deriv<1,0,0>(p, i)*2*dz/3 - p.move(i,0,0,-2)/3 + 4*p.move(i,0,0,-1)/3;
 #ifndef NDEBUG
                         if (!std::isfinite(p(i))) {
                             std::cerr << "pressure boundary condition returned an infinite value\n";
                         }
 #endif
-                    } else {
-                        //p(p.get_move_ind(i,0,0,1)) = -ny/nz * smart_deriv<0,1,0>(p, i)*dz - nx/nz * smart_deriv<1,0,0>(p, i)*dz + p.move(i,0,0,-1);
+                    } else {  //forward difference
                         p(i) = ny/nz* smart_deriv<0,1,0>(p, i)*2*dz/3 + nx/nz* smart_deriv<1,0,0>(p, i)*2*dz/3 - p.move(i,0,0,2)/3 + 4*p.move(i,0,0,1)/3;
 #ifndef NDEBUG
-                        if (!std::isfinite(p.move(i,0,0,1))) {
+                        if (!std::isfinite(p(i))) {
                             std::cerr << "pressure boundary condition returned an infinite value\n";
                         }
 #endif
