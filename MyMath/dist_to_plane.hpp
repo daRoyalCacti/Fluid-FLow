9bbbc4140ac5962c94493c53b29289a48b9e1142
daRoyalCacti <srdrmrdave@gmail.com>
2021-10-10 14:44:03 +1000

    Implemented the equation incorrect. d is defined differently in the 2 sources

--- MyMath/dist_to_plane.hpp
+++ MyMath/dist_to_plane.hpp
@@ -7,11 +7,13 @@
 
 #include "vec3.hpp"
 
-constexpr double dist_to_plane(const vec3& x, const vec3 &p1, const vec3 &p2, const vec3 &p3) {
+/*constexpr*/ double dist_to_plane(const vec3& x, const vec3 &p1, const vec3 &p2, const vec3 &p3) {
     //https://tutorial.math.lamar.edu/classes/calcIII/EqnsOfPlanes.aspx
     const auto v1 = p1 - p2;
     const auto v2 = p3 - p2;
     const auto n = cross(v1, v2);
+
+    std::cerr << n << "\n";
 
     const auto a = n.x();
     const auto b = n.y();
@@ -20,7 +22,7 @@ constexpr double dist_to_plane(const vec3& x, const vec3 &p1, const vec3 &p2, co
     const auto d = a*p1.x() + b*p1.y() + c*p1.z();
 
     //https://mathworld.wolfram.com/Point-PlaneDistance.html
-    const auto num = std::abs( a*x.x() + b*x.y() + c*x.z() + d);
+    const auto num = std::abs( a*x.x() + b*x.y() + c*x.z() - d);
     const auto denom = sqrt(a*a + b*b + c*c);
 
     return num/denom;
